ecb9aef770ea1fd018b1d2cf9bfa5fe6d5f4927a
luis229822 <luis22199822@gmail.com>
2024-11-20 12:04:05 -0500

    index.php

--- index.php
+++ index.php
@@ -1,55 +1,71 @@
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Cat醠ogo de clientes</title>
+  <title>Cat谩logo de Clientes</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
-  <div class = "container">
+  <div class="container">
     <div class="jumbotron">
-      <h1 class="display-4">Cat醠ogo de clientes</h1>
-      <p class="lead">Aplicaci髇 de muestra del cat醠ogo de clientes</p>
+      <h1 class="display-4">Cat谩logo de Clientes</h1>
+      <p class="lead">Aplicaci贸n de muestra del cat谩logo de clientes</p>
       <hr class="my-4">
-      <p>Aplicaci髇 de muestra PHP conectada a una base de datos MySQL para enumerar un cat醠ogo de clientes</p>
+      <p>Conectado a una base de datos MySQL para listar los datos de clientes</p>
     </div>
     <table class="table table-striped table-responsive">
       <thead>
         <tr>
+          <th>DNI</th>
           <th>Nombre</th>
-          <th>Calificacion Crediticia</th>
-          <th>Direccion</th>
-          <th>Ciudad</th>
-          <th>Provincia</th>
-          <th>Pais</th>
-          <th>Codigo Postal</th>
+          <th>Apellido</th>
+          <th>Tel茅fono</th>
+          <th>Tipo Cliente</th>
+          <th>Direcci贸n</th>
+          <th>Localidad</th>
+          <th>Email</th>
         </tr>
       </thead>
       <tbody>
         <?php
+        // Conexi贸n a la base de datos
+        $conexion = mysqli_connect(getenv('localhost'), getenv('root'), getenv('123456'), "PRUEBA");
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
+        // Verificar conexi贸n
+        if (!$conexion) {
+          die("Error de conexi贸n: " . mysqli_connect_error());
+        }
 
-        $cadenaSQL = "select * from s_cliente";
+        // Consulta a la tabla Cliente
+        $cadenaSQL = "SELECT * FROM Cliente";
         $resultado = mysqli_query($conexion, $cadenaSQL);
 
-        while ($fila = mysqli_fetch_object($resultado)) {
-         echo "<tr><td> " .$fila->nombre . 
-         "</td><td>" . $fila->calificacion_crediticia .
-         "</td><td>" . $fila->direccion .
-         "</td><td>" . $fila->ciudad .
-         "</td><td>" . $fila->provincia .
-         "</td><td>" . $fila->pais .
-         "</td><td>" . $fila->codigo_postal .
-         "</td></tr>";
-       }
-       ?>
-     </tbody>
-   </table>
- </div>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-</body>
-</html>
+        // Verificar si hay resultados
+        if (mysqli_num_rows($resultado) > 0) {
+          // Mostrar los datos en la tabla
+          while ($fila = mysqli_fetch_object($resultado)) {
+            echo "<tr>
+              <td>{$fila->DNI}</td>
+              <td>{$fila->Nombre_cliente}</td>
+              <td>{$fila->Apellido}</td>
+              <td>{$fila->Telefono}</td>
+              <td>{$fila->Tipo_cliente}</td>
+              <td>{$fila->Direccion_cl}</td>
+              <td>{$fila->Localidad}</td>
+              <td>{$fila->Email}</td>
+            </tr>";
+          }
+        } else {
+          echo "<tr><td colspan='8'>No se encontraron resultados</td></tr>";
+        }
 
+        // Cerrar conexi贸n
+        mysqli_close($conexion);
+        ?>
+      </tbody>
+    </table>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorig
