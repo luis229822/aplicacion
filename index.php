bd50cfef2d1bef6b8370fff94292965b537cb32c
luis229822 <luis22199822@gmail.com>
2024-11-20 12:17:33 -0500

    index.php

--- index.php
+++ index.php
@@ -30,11 +30,11 @@
       <tbody>
         <?php
         // Conexión a la base de datos
-        $conexion = mysqli_connect(getenv('localhost'), getenv('root'), getenv('123456'), "PRUEBA");
+        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
 
         // Verificar conexión
         if (!$conexion) {
-          die("Error de conexión: " . mysqli_connect_error());
+          die("<tr><td colspan='8'>Error de conexión: " . mysqli_connect_error() . "</td></tr>");
         }
 
         // Consulta a la tabla Cliente
@@ -68,4 +68,6 @@
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorig
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
